2ae3822f7547481937a9fd2e2e50c20f1b9c36ce
robertc181 <rdancole@gmail.com>
2021-01-13 12:29:47 +0000

    addded js JSON

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 var xhr = new XMLHttpRequest();
 
+xhr.open("get", "https://ci-swapi.herokuapp.com/api/");
+
+xhr.send();
+
+function setData(jsonData){
+    data = jsonData;
+    console.log(data)
+}
+
 xhr.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText
+        data = this.responseText;
+        setData(JSON.parse(this.responsetext));
     }
 };
 
-xhr.open("get", "https://swapi.co/api/");
-
-xhr.send()
+console.log(data)
