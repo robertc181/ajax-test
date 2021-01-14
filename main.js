b403725653a1bb41b6b6349896c9cce5e5b45216
robertc181 <rdancole@gmail.com>
2021-01-14 15:21:58 +0000

    added functionality to js

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-var xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("get", "https://ci-swapi.herokuapp.com/api/");
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 
-function setData(jsonData){
-    data = jsonData;
-    console.log(data)
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
-xhr.onreadystatechange = function(){
-    if (this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
-        setData(JSON.parse(this.responsetext));
-    }
-};
+function getTableHeaders(obj) {
+    var tableHeaders = [];
 
-console.log(data)
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            // el.innerHTML += "<p>" + item.name + "</p>";
+        });
+
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
